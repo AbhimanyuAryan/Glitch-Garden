24731f6467f6d5b4d8c43c1951b5566debbc8270
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2017-05-24 16:45:51 +0530

    Setting Triggers & Rigidbody2D to all the attackers & defenders. Bug (have to add Rigidbody on Axe & Corgette). Ben didn't add it.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,11 +5,16 @@ public class Attacker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+		myRigidbody.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log(name + "trigger enter");
 	}
 }
