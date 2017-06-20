f75bcfdd0426f09b8855625e612466ce5cb3c863
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2017-06-20 18:45:01 +0530

    Adding Animation Event to Attackers

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 
 public class Attacker : MonoBehaviour {
-	[Range(-1f, 1.5f)] public float walkSpeed;
-
+	[Range(-1f, 1.5f)] public float currentSpeed; // C# annotation
+	
 	// Use this for initialization
 	void Start () {
 		Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
@@ -11,10 +11,18 @@ public class Attacker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+		transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
 	}
 
 	void OnTriggerEnter2D(Collider2D col){
 		Debug.Log(name + "trigger enter");
+	}
+
+	public void SetSpeed(float speed){
+		currentSpeed = speed;
+	}
+
+	public void StrikeCurrentTarget(float damage){
+		Debug.Log(name + " caused damage: " + damage); 
 	}
 }
--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -6,7 +6,7 @@ public class Defender : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+				
 	}
 	
 	// Update is called once per frame
